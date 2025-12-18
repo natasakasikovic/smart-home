cc46918ee6d006253721fec09a69d5b378beb830
Natasa Kasikovic <nkasikovic@yahoo.com>
2025-12-18 15:52:25 +0100

    chore: update inital folder structure

--- main.py
+++ main.py
@@ -3,6 +3,11 @@ import json
 def load_settings(filePath='settings.json'):
     with open(filePath, 'r') as f:
         return json.load(f)
-    
-if __name__ == "__main__":
+
+def start_pi1():
     pass
+
+if __name__ == "__main__":
+    print('Starting app..')
+    settings = load_settings()
+    threads = []
