8d7ae2db61fddecc2cfa38c367bc385a07f2e1dc
Natasa Kasikovic <nkasikovic@yahoo.com>
2025-12-23 03:30:22 +0100

    feat: add simulated DUS

---
+++ hardware/real/dus.py
@@ -0,0 +1,13 @@
+from hardware.base.dus_interface import DUSInterface
+
+def loop(dus):
+    pass
+
+class DUS(DUSInterface):
+    def __init__(self, config, stop_event, callback):
+        super().__init__(config, stop_event, callback)
+        self.log("Initializing REAL DUS (Door Ultrasonic Sensor) on pin {}".format(self.config.get('pin', 'N/A')))
+
+    def detect(self):
+        print("Read DUS not implemented yet")
+        pass
