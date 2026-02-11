3beb63f755b3a170aab49a71336b2548e2ae17f8
Natasa Kasikovic <nkasikovic@yahoo.com>
2026-02-11 02:57:00 +0100

    feat: add pi3.py

---
+++ controllers/pi3/pi3.py
@@ -0,0 +1,62 @@
+import threading
+import signal
+
+from publisher import Publisher
+from utils.config_loader import load_config
+from components.gsg_manager import GSGManager
+from components.dpir_manager import DPIRManager
+
+def start_sensors(config, stop_event, publisher):
+    threads = []
+    
+    if "DPIR3" in config:
+        dpir1_config = config["DPIR3"]
+        dpir1_config["code"] = "DPIR3"
+        threads.append(
+            DPIRManager.start_dpir(dpir1_config, stop_event, publisher)
+        )
+    
+    return threads
+
+def start_actuators(config, stop_event, publisher):
+    return []
+
+
+def run():
+    print("[PI3] Starting...")
+
+    config = load_config("config/settings.json")
+    pi_config = config["PI3"]
+
+    stop_event = threading.Event()
+
+    signal.signal(signal.SIGINT, lambda s, f: stop_event.set())
+    signal.signal(signal.SIGTERM, lambda s, f: stop_event.set())
+
+    publisher = Publisher(config["mqtt"])
+    publisher.start_daemon()
+
+    threads = start_sensors(pi_config, stop_event, publisher)
+    actuators = start_actuators(pi_config, stop_event, publisher)
+
+    print("[PI3] System ready.")
+
+    try:
+        while not stop_event.is_set():
+            # TODO: add command handler
+            stop_event.wait(0.5)
+
+    except KeyboardInterrupt:
+        stop_event.set()
+        publisher.stop_event.set()
+
+    finally:
+        print("[PI3] Stopping publisher...")
+        publisher.shutdown()
+
+        print("[PI3] Waiting for threads to finish...")
+        for t in threads:
+            if t.is_alive():
+                t.join(timeout=2)
+
+        print("[PI3] Shutdown complete")
